18c84b13ea6fb683da1d5c295da696399f011607
Daria Gortchacow <daria.gortchacow@gmail.com>
2022-11-19 21:09:59 +0100

    show movies list

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -7,9 +7,9 @@ export default class extends Controller {
 
   updateNavbar() {
     if (window.scrollY >= window.innerHeight) {
-      this.element.classList.add("navbar-lewagon-white")
-    } else {
       this.element.classList.remove("navbar-lewagon-white")
+    } else {
+      this.element.classList.add("navbar-lewagon-white")
     }
   }
 }
